38bfb5e1375131383bce2dbcfae8b515026764c9
Peter <xutongle@gmail.com>
2017-11-19 18:39:20 +0800

    update

--- models/AttentionQuery.php
+++ models/AttentionQuery.php
@@ -9,6 +9,28 @@ namespace yuncms\attention\models;
  */
 class AttentionQuery extends \yii\db\ActiveQuery
 {
+    /**
+     * @var string 模型类型
+     */
+    public $model_class;
+
+    /**
+     * @var string 数据表名称
+     */
+    public $tableName;
+
+    /**
+     * @param \yii\db\QueryBuilder $builder
+     * @return $this|\yii\db\Query
+     */
+    public function prepare($builder)
+    {
+        if (!empty($this->model_class)) {
+            $this->andWhere([$this->tableName . '.model_class' => $this->model_class]);
+        }
+        return parent::prepare($builder);
+    }
+
     /*public function active()
     {
         return $this->andWhere('[[status]]=1');
